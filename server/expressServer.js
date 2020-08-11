7948ed58443aa327b36e4a1e8ee7e917d13512e9
Andrew Thresher <thresh3@me.com>
2020-08-10 23:40:04 -0500

    deployment changes

--- server/expressServer.js
+++ server/expressServer.js
@@ -1,9 +1,4 @@
 const express = require('express');
-const webpack = require('webpack');
-const webpackDevMiddleware = require('webpack-dev-middleware');
-const webpackHotMiddleware = require('webpack-hot-middleware');
-const webpackConfig = require('../webpack.config.js');
-const webpackCompiler = webpack(webpackConfig);
 const app = express();
 
 global.DIR = __dirname;
@@ -16,14 +11,21 @@ require(DIR + '/load/mongoose');
 Logger.log('Loaded configuration');
 
 // Webpack middleware
-app.use(webpackDevMiddleware(webpackCompiler, {
-  publicPath: webpackConfig.output.publicPath,
-  stats: { ...webpackConfig.stats }
-}));
-app.use(webpackHotMiddleware(webpackCompiler, {
-  log: console.log,
-  path: '/__webpack_hmr'
-}));
+if(ENVIRONMENT === 'dev'){
+  const webpack = require('webpack');
+  const webpackDevMiddleware = require('webpack-dev-middleware');
+  const webpackHotMiddleware = require('webpack-hot-middleware');
+  const webpackConfig = require('../webpack.config.js');
+  const webpackCompiler = webpack(webpackConfig);
+  app.use(webpackDevMiddleware(webpackCompiler, {
+    publicPath: webpackConfig.output.publicPath,
+    stats: { ...webpackConfig.stats }
+  }));
+  app.use(webpackHotMiddleware(webpackCompiler, {
+    log: console.log,
+    path: '/__webpack_hmr'
+  }));
+}
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/../public/`));
