ccac85ec7a68be3ac6f3b8ff5ceca207c28ca412
Andrew Thresher <thresh3@me.com>
2020-09-20 13:55:34 -0500

    Fixed fields empty string pass bug

--- server/validations/fields.js
+++ server/validations/fields.js
@@ -2,7 +2,7 @@ module.exports = (inputs, required) => {
     return new Promise((resolve, reject) => {
         let notFound = [];
         required.forEach(field => {
-            if(typeof inputs[field] == 'undefined') notFound.push("'" + field + "'" + " field is required");
+            if(typeof inputs[field] == 'undefined' || inputs[field] == '') notFound.push("'" + field + "'" + " field is required");
         });
         if(notFound.length > 0) reject({'required': notFound});
         else resolve();
