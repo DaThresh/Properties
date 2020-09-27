e205fe3b36cd398df68f6b931cb8ba6fa3553815
Andrew Thresher <thresh3@me.com>
2020-09-27 01:08:23 -0500

    Added admin middleware, modified manager middleware error

--- server/middleware/verifyAdmin.js
+++ server/middleware/verifyAdmin.js
@@ -1,4 +1,4 @@
 module.exports = (req, res, next) => {
-    if(req.account.role > global.managerRole) next();
+    if(req.account.organization.toLocaleString() === global.adminOrganization) next();
     else Errors.response(res, {authorization: true});
 }
