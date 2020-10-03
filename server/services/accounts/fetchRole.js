cd67046f9637fe9ed8c2705f31f8f2046a4fc06a
Andrew Thresher <thresh3@me.com>
2020-10-02 22:30:48 -0500

    Added organization name to account ref data, added countup

--- server/services/accounts/fetchRole.js
+++ server/services/accounts/fetchRole.js
@@ -1,10 +1,13 @@
 const success = require('../success');
 
 module.exports = (req, res) => {
-    success(res, {
-        role: req.account.role,
-        admin: req.account.organization.equals(global.adminOrganization),
-        userName: req.account.fullName,
+    req.account.populate('organization').execPopulate()
+    .then(account => success(res, {
+        role: account.role,
+        admin: account.organization._id.equals(global.adminOrganization),
+        organizationName: account.organization.name,
+        userName: account.fullName,
         managerRole: global.managerRole,
-    });
+    }))
+    .catch(error => Errors.response(res, error));
 }
