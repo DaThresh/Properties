b879bfc295a757e2394af58667174f6c9c3027b8
Andrew Thresher <thresh3@me.com>
2020-10-05 20:43:02 -0500

    Fixed pagination bugs

--- client/shared/Pagination.jsx
+++ client/shared/Pagination.jsx
@@ -8,25 +8,15 @@ function Pagination(props){
             <nav className="pagination is-small" role="navigation" aria-label="pagination">
                 {page > 1 ? <a className="pagination-previous" onClick={changePage} data-number={page - 1}>Previous</a> : null }
                 {page < pageCount ? <a className="pagination-next" onClick={changePage} data-number={page + 1}>Next</a> : null }
-                {pageCount > 1 ? 
-                    <ul className="pagination-list">
-                        {page >= 3 ?
-                            <span>
-                                <li><a className="pagination-link" onClick={changePage} data-number={1}>1</a></li>
-                                <li><span className="pagination-ellipsis">&hellip;</span></li>
-                            </span>
-                        : null }
-                        {page > 1 ? <li><a className="pagination-link" onClick={changePage} data-number={page - 1}>{page - 1}</a></li> : null }
-                        <li><a className="pagination-link is-current">{page}</a></li>
-                        {page < pageCount ? <li><a className="pagination-link" onClick={changePage} data-number={page + 1}>{page + 1}</a></li> : null }
-                    </ul>
-                : null }
-                {pageCount >= page + 3 ?
-                    <span>
-                        <li><span className="pagination-ellipsis">&hellip;</span></li>
-                        <li><a className="pagination-link" onClick={changePage} data-number={pageCount}>{pageCount}</a></li>
-                    </span>
-                : null }
+                <ul className="pagination-list">
+                    {page >= 3 ? <li><a className="pagination-link" onClick={changePage} data-number={1}>1</a></li> : null }
+                    {page > 3 ? <li><span className="pagination-ellipsis">&hellip;</span></li> : null }
+                    {page > 1 ? <li><a className="pagination-link" onClick={changePage} data-number={page - 1}>{page - 1}</a></li> : null }
+                    <li><a className="pagination-link is-current">{page}</a></li>
+                    {page < pageCount ? <li><a className="pagination-link" onClick={changePage} data-number={page + 1}>{page + 1}</a></li> : null }
+                    {pageCount > page + 2 ? <li><span className="pagination-ellipsis">&hellip;</span></li> : null }
+                    {pageCount >= page + 2 ? <li><a className="pagination-link" onClick={changePage} data-number={pageCount}>{pageCount}</a></li> : null }
+                </ul>
             </nav>
         </div>
     )
