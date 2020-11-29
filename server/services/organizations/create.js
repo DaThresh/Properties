c0c00330765b76dc884f0e925e2a28b09b0289cd
Andrew Thresher <thresh3@me.com>
2020-11-29 10:53:24 -0600

    Changes to success handling, added org creation log

--- server/services/organizations/create.js
+++ server/services/organizations/create.js
@@ -6,5 +6,6 @@ module.exports = (req, res) => {
     fields(req.body, ['name'])
     .then(() => Organization.create({name: String(req.body.name)}))
     .then(organization => success(res, {organization}, 201))
+    .then(body => Logger.log('Created new organization with ID ' + body.organization.id))
     .catch(error => Errors.response(res, error));
 }
--- server/services/success.js
+++ server/services/success.js
@@ -1,3 +1,4 @@
 module.exports = (response, body = {}, code = 200) => {
     response.status(code).json({ ...body, ...{message: 'success'} })
+    return body;
 }
